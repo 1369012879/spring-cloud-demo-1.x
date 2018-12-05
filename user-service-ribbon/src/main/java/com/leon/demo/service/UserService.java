894a781dba6c005efbdbafb4e66cb6c3197bc612
Leon <1369012879@qq.com>
2018-12-05 14:56:20 +0800

    增加user-service-ribbon模块

---
+++ user-service-ribbon/src/main/java/com/leon/demo/service/UserService.java
@@ -0,0 +1,11 @@
+package com.leon.demo.service;
+
+/**
+ * UserService
+ *
+ * @author leon
+ * @date 2018/12/4
+ */
+public interface UserService {
+
+}
---
+++ user-service-ribbon/src/main/java/com/leon/demo/service/impl/UserServiceImpl.java
@@ -0,0 +1,15 @@
+package com.leon.demo.service.impl;
+
+import com.leon.demo.service.UserService;
+import org.springframework.stereotype.Service;
+
+/**
+ * UserServiceImpl
+ *
+ * @author leon
+ * @date 2018/12/4
+ */
+@Service
+public class UserServiceImpl implements UserService {
+
+}
